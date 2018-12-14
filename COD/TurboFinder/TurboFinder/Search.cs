f5c1d6c0e03f20a95dd48385dc8c5074346e3db2
PhilCPNV <philippe.baumann@cpnv.ch>
2018-12-14 18:29:40 +0100

    Final Pushing
    Commentaire Finition

--- COD/TurboFinder/TurboFinder/Search.cs
+++ COD/TurboFinder/TurboFinder/Search.cs
@@ -2,12 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.AccessControl;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using DocumentFormat.OpenXml.Spreadsheet;
@@ -18,47 +13,33 @@ namespace TurboFinder
     {
         public List<string> Searching(string Folder, string SearchTerm, string SearchFilter)
         {
-            //Initialisze string
+            // Initialisze a empty string list
             List<string> files = new List<string>();
 
             // Take a snapshot of the file system.  
             System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Folder);
 
-            // This method assumes that the application has discovery permissions  
-            // for all folders under the specified path.
-
+            // This method assumes that the application has discovery permissions for all folders under the specified path 
             IEnumerable<System.IO.FileInfo> fileList;
 
+            // Testing the permissions and gathering the filelist for the selected search folder
             try
             {
                 fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
             }
             catch (Exception)
             {
-                MessageBox.Show("Vous ne possèdez pas les droits néssésaire pour certains fichier ou dossier.");
-                return null;
+                return null; // Abord the search
             }
 
-            // Search the contents of each file.  
-            // A regular expression created with the RegEx class  
-            // could be used instead of the Contains method.  
-            /* queryMatchingFiles is an IEnumerable<string>.  
-            var queryMatchingFiles =
-                from file in fileList
-                where file.Extension == ".*"
-                let fileText = GetFileText(file.FullName)
-                where fileText.Contains(SearchTerm)
-                select file.FullName;
-            */
-
-            //Create the query  
-
+            // Initialisze a empty string list and gather the different extensions into string tables
             List<string> extentions = new List<string>();
 
             string[] extensionsIMG = { ".png", ".jpg", ".gif" };
             string[] extensionsVDO = { ".mp4, .avi, .wmv, .mov, .flv" };
             string[] extensionsMSC = { ".aac, .mp3, .ogg, .wav, .flac" };
 
+            // Load the right extension for the specific filter that has to be applied
             switch (SearchFilter)
             {
                 case "Extentions":
@@ -84,6 +65,9 @@ namespace TurboFinder
                     break;
             }
 
+            // For special files use a custom "decoding" methode and for any other files use the basic one located at the end
+            // Each SwitchCase consists of a specifically designed filter powered by Linq, files are sorted and selected then added to the "files" StringList
+
             if (SearchFilter == "Standard")
             {
                 IEnumerable<System.IO.FileInfo> fileQuery =
@@ -97,7 +81,7 @@ namespace TurboFinder
             {
                 IEnumerable<System.IO.FileInfo> fileQuery =
                 from file in fileList
-                where file.LastWriteTime.Month.ToString() == SearchTerm || file.LastWriteTime.Day.ToString() == SearchTerm || file.LastWriteTime.Year.ToString() == SearchTerm
+                where file.LastWriteTime.Month.ToString() == SearchTerm || file.LastWriteTime.Day.ToString() == SearchTerm || file.LastWriteTime.Year.ToString() == SearchTerm || file.LastWriteTime.Date.ToShortDateString() == SearchTerm
                 orderby file.Name
                 select file;
                 files = AddFiles(fileQuery);
@@ -107,7 +91,7 @@ namespace TurboFinder
                 IEnumerable<System.IO.FileInfo> fileQuery =
                 from file in fileList
                 where file.Extension == ".pdf"
-                let fileText = GetFileText(file.FullName)
+                let fileText = GetFileText(file.FullName) // Custom Text Extractor
                 where fileText.Contains(SearchTerm)
                 orderby file.Name
                 select file;
@@ -118,7 +102,7 @@ namespace TurboFinder
                 IEnumerable<System.IO.FileInfo> fileQuery =
                 from file in fileList
                 where file.Extension == ".docx" || file.Extension == ".doc"
-                let fileText = GetWordText(file.FullName)
+                let fileText = GetWordText(file.FullName) // Custom Text Extractor
                 where fileText.Contains(SearchTerm)
                 orderby file.Name
                 select file;
@@ -129,7 +113,7 @@ namespace TurboFinder
                 IEnumerable<System.IO.FileInfo> fileQuery =
                 from file in fileList
                 where file.Extension == ".xlsx" || file.Extension == ".xls"
-                let fileText = GetExcelText(file.FullName)
+                let fileText = GetExcelText(file.FullName) // Custom Text Extractor
                 where fileText.Contains(SearchTerm)
                 orderby file.Name
                 select file;
@@ -145,12 +129,12 @@ namespace TurboFinder
                 files = AddFiles(fileQuery);
             }
 
-            return files;
+            return files; // Collect all the found files and return them
         }
 
         private List<string> AddFiles(IEnumerable<FileInfo> fileQuery)
         {
-            //Initialisze string
+            // Initialisze a new empty string
             List<string> files = new List<string>();
 
             // Execute the query
@@ -159,28 +143,29 @@ namespace TurboFinder
                 files.Add(fi.FullName);
             }
 
+            // Return result
             return files;
         }
-
-        // Read the contents of the file.  
+        
         static string GetFileText(string name)
         {
+            // Initialisze a new empty string
             string fileContents = String.Empty;
 
-            // If the file has been deleted since we took   
-            // the snapshot, ignore it and return the empty string.  
+            // Verify if the given file still exists or skip this step
             if (System.IO.File.Exists(name))
             {
-                fileContents = System.IO.File.ReadAllText(name);
+                fileContents = System.IO.File.ReadAllText(name); // Read all the content of the given File 
             }
-            MessageBox.Show(fileContents);
             return fileContents;
         }
 
         static string GetWordText(string filename)
         {
+            // Try to extract all the words inside of a MS Word document
             try
             {
+                // Create a new stream process and extract the BodyText of the document inside a string
                 Stream stream = File.Open(filename, FileMode.Open);
                 WordprocessingDocument wrdprodoc = WordprocessingDocument.Open(stream, true);
                 Body body = wrdprodoc.MainDocumentPart.Document.Body;
@@ -189,48 +174,54 @@ namespace TurboFinder
             }
             catch (Exception)
             {
-                return "";
+                return ""; // Return an empty string if the File could not be parsed
             }
         }
 
         static string GetExcelText(string filename)
         {
-            string content = "";
-
-            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            // Try to extract all the words inside of a MS Excel document
+            try
             {
-                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
+                // Initialisze a new empty string
+                string content = String.Empty;
+
+                // Create a new stream process and extract all the cells content
+                using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
-                    WorkbookPart workbookPart = doc.WorkbookPart;
-                    SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
-                    SharedStringTable sst = sstpart.SharedStringTable;
-
-                    WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-                    Worksheet sheet = worksheetPart.Worksheet;
-
-                    var cells = sheet.Descendants<Cell>();
-                    var rows = sheet.Descendants<Row>();
-
-                    Console.WriteLine("Row count = {0}", rows.LongCount());
-                    Console.WriteLine("Cell count = {0}", cells.LongCount());
-
-                    foreach (Cell cell in cells)
+                    using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
                     {
-                        if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
+                        WorkbookPart workbookPart = doc.WorkbookPart;
+                        SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                        SharedStringTable sst = sstpart.SharedStringTable;
+
+                        WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                        Worksheet sheet = worksheetPart.Worksheet;
+
+                        var cells = sheet.Descendants<Cell>();
+
+                        // Cycle trough all the cells individually and store the extracted content into the content string
+                        foreach (Cell cell in cells)
                         {
-                            int ssid = int.Parse(cell.CellValue.Text);
-                            string str = sst.ChildElements[ssid].InnerText;
-                            content = content + str;
-                        }
-                        else if (cell.CellValue != null)
-                        {
-                            content = content + cell.CellValue.Text;
+                            if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
+                            {
+                                int ssid = int.Parse(cell.CellValue.Text);
+                                string str = sst.ChildElements[ssid].InnerText;
+                                content = content + str;
+                            }
+                            else if (cell.CellValue != null)
+                            {
+                                content = content + cell.CellValue.Text;
+                            }
                         }
                     }
                 }
+                return content;
             }
-            MessageBox.Show(content);
-            return content;
+            catch (Exception)
+            {
+                return ""; // Return an empty string if the File could not be parsed
+            }
         }
     }
 }
